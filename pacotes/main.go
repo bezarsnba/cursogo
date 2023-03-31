2c313a6d66b121f688539440c570bd9e8b24d658
Bezaleel Silva <bezarsnba@gmail.com>
2023-03-31 15:05:36 -0300

    aula pacote

--- pacotes/main.go
+++ pacotes/main.go
@@ -1,12 +1,16 @@
 package main
 
-import "fmt"
-import ""
-
+import (
+	"fmt"
+	
+	 "./prefixo"
+	 "./operadora"
+)
 // NomeDousuario  é um nome do usuario
 var NomeDoUsuario = "Bob"
 
 func main() {
 	fmt.Printf("Nome do usuario: %s\r\n", NomeDoUsuario)
 	fmt.Printf("Prefixo da capitao: %d\r\n", prefixo.Capital)
+	fmt.Printf("Nome da operadora: %s\r\n", operadora.NomeOperadora)
 }
--- pacotes/prefixo/numero.go
+++ pacotes/prefixo/numero.go
@@ -1,4 +1,6 @@
 package prefixo
+
+
 //Capital representa o numero do prefixo de telefone da capital de um estado 
 var Capital = 11
 
